3ae3559b371de1f25f689f03e569083236e48356
Steven Silvester <steven.silvester@ieee.org>
2014-10-12 20:28:14 -0500

    Bump the version

--- tifffile.py
+++ tifffile.py
@@ -153,7 +153,7 @@ import numpy
 
 import _tifffile
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __docformat__ = 'restructuredtext en'
 __all__ = ('imsave', 'imread', 'imshow', 'TiffFile', 'TiffWriter',
            'TiffSequence')
